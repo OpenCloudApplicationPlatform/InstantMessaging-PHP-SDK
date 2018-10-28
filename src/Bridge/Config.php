0ec379f796c120a8f112b3e35e187441a791e666
刘广财 <itxiao6@qq.com>
2018-10-28 22:27:27 +0800

    SDK配置

---
+++ src/Bridge/Config.php
@@ -0,0 +1,18 @@
+<?php
+namespace OCAP\InstantMessaging\Bride;
+/**
+ * 包的配置
+ * Class Config
+ * @package OCAP\InstantMessaging\Bride
+ */
+class Config
+{
+    /**
+     * API 版本
+     */
+    CONST API_VERSION = '0.0.0.1';
+    /**
+     * SDK 版本
+     */
+    CONST SDK_VERSION = '0.0.0.1';
+}
