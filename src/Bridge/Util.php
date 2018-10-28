2e0ef7478b2207d26258ae44cac9b13dd199ab63
刘广财 <itxiao6@qq.com>
2018-10-28 21:37:16 +0800

    零碎工具包

---
+++ src/Bridge/Util.php
@@ -0,0 +1,27 @@
+<?php
+namespace OCAP\InstantMessaging\Bride;
+/**
+ * Class Util
+ * @package OCAP\InstantMessaging\Bride
+ */
+class Util
+{
+    /**
+     * 过滤微信昵称中的表情（不过滤 HTML 符号）
+     */
+    public static function filterNickname($nickname)
+    {
+        $pattern = array(
+            '/\xEE[\x80-\xBF][\x80-\xBF]/',
+            '/\xEF[\x81-\x83][\x80-\xBF]/',
+            '/[\x{1F600}-\x{1F64F}]/u',
+            '/[\x{1F300}-\x{1F5FF}]/u',
+            '/[\x{1F680}-\x{1F6FF}]/u',
+            '/[\x{2600}-\x{26FF}]/u',
+            '/[\x{2700}-\x{27BF}]/u',
+            '/[\x{20E3}]/u'
+        );
+        $nickname = preg_replace($pattern, '', $nickname);
+        return trim($nickname);
+    }
+}
