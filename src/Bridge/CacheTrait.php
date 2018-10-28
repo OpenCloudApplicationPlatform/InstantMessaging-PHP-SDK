078097c7ea4201e6b860cfe86aaa8e35046e168d
刘广财 <itxiao6@qq.com>
2018-10-28 22:27:20 +0800

    缓存辅助类

--- src/Bridge/CacheTrait.php
+++ src/Bridge/CacheTrait.php
@@ -8,21 +8,29 @@ namespace OCAP\InstantMessaging\Bride;
 trait CacheTrait
 {
     /**
-     * Doctrine\Common\Cache\Cache
+     * @var \Doctrine\Common\Cache\Cache
      */
-    protected $cache;
+    protected $cache = null;
+
     /**
      * 设置缓存驱动
+     * @param \Doctrine\Common\Cache\Cache $cache
      */
     public function setCache(\Doctrine\Common\Cache\Cache $cache)
     {
         $this->cache = $cache;
     }
+
     /**
      * 获取缓存驱动
+     * @return \Doctrine\Common\Cache\Cache
+     * @throws \Exception
      */
     public function getCache()
     {
+        if($this -> cache === null){
+            throw new \Exception('请先设置缓存驱动');
+        }
         return $this->cache;
     }
 }
